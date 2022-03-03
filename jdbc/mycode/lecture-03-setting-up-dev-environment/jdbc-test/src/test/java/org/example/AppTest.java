ad869bf04726277c4870e461f4a554d830bdf8a2
Eric Ricketts <eric_ricketts@me.com>
2022-03-02 19:11:02 -0500

    first commit

---
+++ jdbc/mycode/lecture-03-setting-up-dev-environment/jdbc-test/src/test/java/org/example/AppTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.function.BooleanSupplier;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
